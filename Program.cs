d89644580899b573003f94f109d5fa595b1cfcb8
saltkeeper1@gmail.com <saltkeeper1@gmail.com>
2020-06-23 15:40:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+            tree.Add(4);
+            tree.Add(0);
+
+            foreach (var item in tree.Preorder())
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+            foreach (var item in tree.Postorder())
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+            foreach (var item in tree.Inorder())
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
